4ffc89d3738cbe8f4aca5c3303fe2bd00f34c407
Silvio Limeira <silvio.limeira@live.com>
2019-06-20 12:06:30 -0300

    update notaS.txt

--- src/app/services/state/state.service.ts
+++ src/app/services/state/state.service.ts
@@ -6,5 +6,7 @@ import { Injectable } from '@angular/core';
 export class StateService {
 
   //mensagem = "mensagem state service";
-  constructor() { }
+  constructor() { 
+    console.log('create StateService');
+  }
 }
