9a25efb32bbfa7d360a65d0bb4045f8ac2386ab2
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-27 09:19:01 -0300

    add getStarwarsCharactersStream(), operator from call said function into a stream

--- stream-in-a-stream/app.js
+++ stream-in-a-stream/app.js
@@ -1,16 +1,24 @@
 // https://github.com/ReactiveX/rxjs#cdn
 
-const { fromEvent } = rxjs;
-const { map, filter } = rxjs.operators;
+const { fromEvent, observable, from } = rxjs;
+const { map, filter, mergeMap } = rxjs.operators;
 
 let elem = document.getElementById("input");
 
-let keyStream$ = fromEvent(elem, "keyup").pipe(map(ev => ev.key));
+function getStarwarsCharactersStream(id) {
+  return fetch("https://swapi.co/api/people/" + id).then(data => data.json());
+}
+
+let keyStream$ = fromEvent(elem, "keyup").pipe(
+  map(ev => ev.key),
+  filter(key => key !== "Backspace"),
+  mergeMap(key => from(getStarwarsCharactersStream(key)))
+);
 
 keyStream$.subscribe(key => console.log(key));
 
-fetch("https://swapi.co/api/people/1")
-  .then(data => data.json())
-  .then(data => console.log("data: ", data));
+// fetch("https://swapi.co/api/people/1")
+//   .then(data => data.json())
+//   .then(data => console.log("data: ", data));
 
 // emits entered key chars
