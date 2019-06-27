06ab7b26f58b8441bb14945b27ba20f65874eb97
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-27 07:37:41 -0300

    working with static values and observer using pipe(map and filter) updating template layout

--- src/app/components/filterable-observable/filterable-observable.component.ts
+++ src/app/components/filterable-observable/filterable-observable.component.ts
@@ -5,6 +5,7 @@ import {
   MyObservable as MyObservable2
 } from "../../rxjs-core/operator/observable";
 import { Observable, Observer, of } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
 @Component({
   selector: "app-filterable-observable",
@@ -32,27 +33,33 @@ export class FilterableObservableComponent implements OnInit {
   // }
 
   ngOnInit() {
-    // this.numbers.push(1);
-    // this.numbers.push(2);
-    // this.numbers.push(3);
-    // console.log("numbers: ", this.numbers);
+    // first one test
+    // let stream$ = new MyObservable2(observer => {
+    //   observer.next(1);
+    //   observer.next(2);
+    //   observer.next(3);
+    //   observer.next(4);
+    //   observer.next(5);
+    // }).filter(x => x > 3);
+    // stream$.subscribe(data => {
+    //   console.log("FilterableObservable, MyObservable2 - data*: ", data);
+    //   this.numbers.push(data);
+    //   // this.sequence = of(this.numbers);
+    // });
 
-    let stream$ = new MyObservable2(observer => {
-      observer.next(1);
-      observer.next(2);
-      observer.next(3);
-      observer.next(4);
-      observer.next(5);
-    }).filter(x => x > 3);
-    stream$.subscribe(data => {
-      console.log("FilterableObservable, MyObservable2 - data*: ", data);
-      this.numbers.push(data);
-      // this.sequence = of(this.numbers);
-    });
+    // working with observer and static values
+    // const staticValuesStream$ = of(1, 22, 333, 4444);
+    // staticValuesStream$.subscribe(data => {
+    //   console.log("staticValuesStream$, value: ", data);
+    //   this.numbers.push(data);
+    //   this.sequence = of(this.numbers);
+    // });
 
-    const staticValuesStream$ = of(1, 22, 333, 4444);
+    // working with static values and observer using pipe(map and filter)
+    const staticValuesStream$ = of(1, 2, 3, 4, 5)
+      .pipe(map(data => data + 1))
+      .pipe(filter(data => data % 2 === 0));
     staticValuesStream$.subscribe(data => {
-      console.log("staticValuesStream$, value: ", data);
       this.numbers.push(data);
       this.sequence = of(this.numbers);
     });
