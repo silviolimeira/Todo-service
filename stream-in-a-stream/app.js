a4e55acae574deabf4fac7a54b24428326c7306c
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-28 08:42:51 -0300

    add method longRunningTask, process input with delay

--- stream-in-a-stream/app.js
+++ stream-in-a-stream/app.js
@@ -2,7 +2,7 @@
 // https://www.learnrxjs.io/operators/transformation/switchmap.html
 
 const { fromEvent, observable, from } = rxjs;
-const { map, filter, mergeMap } = rxjs.operators;
+const { map, filter, mergeMap, flatMap } = rxjs.operators;
 
 let elem = document.getElementById("input");
 
@@ -10,10 +10,21 @@ function getStarwarsCharactersStream(id) {
   return fetch("https://swapi.co/api/people/" + id).then(data => data.json());
 }
 
+function longRunningTask(input) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log(".");
+      resolve("response based on " + input);
+    }, 5000);
+  });
+}
+
 let keyStream$ = fromEvent(elem, "keyup").pipe(
   map(ev => ev.key),
+  // filter(elem => elem. > 3),
   filter(key => key !== "Backspace"),
-  mergeMap(key => from(getStarwarsCharactersStream(key)))
+  // mergeMap(key => from(getStarwarsCharactersStream(key)))
+  flatMap(key => longRunningTask(elem.value))
 );
 
 keyStream$.subscribe(key => console.log(key));
